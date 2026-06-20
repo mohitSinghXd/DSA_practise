2d999dde741217f7912295bff98d44d34e82ebe6
mohitSinghXd <mohitsingh.builds@gmail.com>
2026-06-20 10:23:45 +0530

    another q

---
+++ basicquestions/Q2.py
@@ -0,0 +1,44 @@
+#check weather a number is palindrome or not 
+
+def check_palindrome(number):
+    num = number  
+    result = 0 
+    while num != 0 :
+        id  = num % 10  
+        result = (result*10)+ id 
+        num = num//10
+    print(result)
+    return result == number
+number = int(input("enter  your number : "))
+result = check_palindrome(number) 
+if result == True:
+    print("palindrome")
+
+else : 
+    print("not palindrome") 
+ 
+
+
+# time complexity = o(log 10 (n))  where n = number of digit  
+#---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+"""" 
+this is also a way but not optimal it takes almost o(n)
+
+
+def check_palindrome(number): 
+    num = number
+    reversed_string = "" 
+    for i in range(len(num)):
+        reversed_string  = num[i] + reversed_string 
+    return reversed_string  == num
+
+
+number = input("enter your number : " )
+
+result = check_palindrome(number) 
+print(result)
+
+"""
